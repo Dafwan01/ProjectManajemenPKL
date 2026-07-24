f3ca23b1081a88a7627d31e4b0f63056d2551be2
Dafwan01 <major8849@gmail.com>
2026-07-24 14:42:44 +0700

    ganti icon

--- resources/views/livewire/components/header.blade.php
+++ resources/views/livewire/components/header.blade.php
@@ -70,14 +70,14 @@
               </a>
            </li>
 
-           <li>
-              <a href="#" wire:click="GoToPermohonanIzin" wire:navigate class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                 <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 10a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"/>
-                 </svg>
-                 <span class="flex-1 ms-3 whitespace-nowrap">Permohonan Izin/Sakit</span>
-              </a>
-           </li>
+         <li>
+    <a href="#" wire:click="GoToPermohonanIzin" wire:navigate class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+        </svg>
+        <span class="flex-1 ms-3 whitespace-nowrap">Permohonan Izin/Sakit</span>
+    </a>
+</li>
            <!-- 6. Log Out -->
            <li>
               <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
