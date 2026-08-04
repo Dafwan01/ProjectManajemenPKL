4cadf92228b88eb1e59ed9d6a4d6ec20365e9e9e
N4H7 <0815nos@gmail.com>
2026-08-04 20:00:12 +0700

    Update Notif Keterlambatan

--- app/Livewire/Header.php
+++ app/Livewire/Header.php
@@ -2,10 +2,42 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Header extends Component
 {
+    public $notifikasi = [];
+    public $jumlahBelumDibaca = 0;
+
+    public function mount()
+    {
+        $this->loadNotifikasi();
+    }
+
+    public function loadNotifikasi()
+    {
+        $user = Auth::user();
+        if (!$user) {
+            return;
+        }
+
+        $this->notifikasi = $user->notifications()->latest()->take(10)->get();
+        $this->jumlahBelumDibaca = $user->unreadNotifications()->count();
+    }
+
+    public function tandaiDibaca($id)
+    {
+        Auth::user()?->notifications()->where('id', $id)->first()?->markAsRead();
+        $this->loadNotifikasi();
+    }
+
+    public function tandaiSemuaDibaca()
+    {
+        Auth::user()?->unreadNotifications->markAsRead();
+        $this->loadNotifikasi();
+    }
+
     public function render()
     {
         return view('livewire.components.header');
